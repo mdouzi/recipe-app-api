0cd0b38aca2349614fe601b157ab0a0b2e32f01f
Mohamed Douzi <mdouzi@c2r9p4.1337.ma>
2024-10-14 06:15:44 +0100

    update

---
+++ app/core/managment/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for the datebase to be avaible.
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+    
+    def hanle(self, *args, **options):
+        pass
